c3c8620085509a695990b85c0d1f7b10554bdd84
Eduardo Perottoni <edu.perottoni@gmail.com>
2023-03-25 17:33:14 -0300

    Inserting T2

---
+++ T2/codigo_fonte/thread.cc
@@ -0,0 +1,24 @@
+#include "thread.h"
+
+__BEGIN_API
+
+int Thread::_thread_counter = 0;
+Thread* Thread::_running = nullptr;
+
+// Thread id: Feito
+int Thread::id(){
+	return _id;
+}
+
+int Thread::switch_context(Thread * prev, Thread * next) {
+    // Thread que está executando será a próxima
+    _running = next;
+    return CPU::switch_context(prev-> context(), next -> context());
+}
+
+void Thread::thread_exit (int exit_code) {
+    delete _context;
+    --_thread_counter;
+}
+
+__END_API
