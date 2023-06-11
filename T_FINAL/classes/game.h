ba9b4e80b1ebe3ab831a71797fbe49124411f64f
Eduardo Perottoni <edu.perottoni@gmail.com>
2023-06-11 18:33:03 -0300

    Porra

--- T_FINAL/classes/game.h
+++ T_FINAL/classes/game.h
@@ -12,10 +12,24 @@ class Game
 private:
     static Thread* _window_thread;
     static Window* _window_obj;
+    static void window_run(){
+        std::cout << "Aqui 4" << std::endl;
+        _window_obj = new Window();
+        std::cout << "Aqui 5" << std::endl;
+        _window_obj->run();
+    }
 public:
-    static void run(void* name);
-    static void window_run();
+    Game() {}
+    ~Game() {}
+    
+    static void run(void* name) {
+        std::cout << "Aqui 2" << std::endl;
+        _window_thread = new Thread(window_run);
+        std::cout << "Aqui 3" << std::endl;
+        _window_thread -> join();
+    }  
 };
+
 
 __END_API
 
