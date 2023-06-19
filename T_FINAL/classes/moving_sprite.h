7ddb95cda4548e464b18c3785b6ec33d4fc203a2
Eduardo Perottoni <edu.perottoni@gmail.com>
2023-06-18 22:28:35 -0300

    Improving encapsulation

--- T_FINAL/classes/moving_sprite.h
+++ T_FINAL/classes/moving_sprite.h
@@ -5,14 +5,35 @@
 #include "../src/lib/traits.h"
 #include "sprite.h"
 #include <tuple>
+#include <map>
+#include <string>
+
 
 __BEGIN_API
 
+using namespace std;
 class MovingSprite : public Sprite {
     public:
+        enum Orientation {
+            UNIQUE,
+            RIGHT,
+            LEFT,
+            UP,
+            DOWN
+        };
+        MovingSprite(const map<Orientation, string>& paths);
+        // ~MovingSprite();
+
         void move();
-    private:
-        int speed;
+        sf::Sprite& get_current_sprite(){
+            return _current_sprite;
+        }
+
+    protected:
+        int _speed;
+        Orientation _orientation;
+        sf::Sprite _current_sprite;
+        map<Orientation, sf::Texture> _textures;
 };
 
 __END_API
